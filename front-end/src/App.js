3d74bcbe3ab687a08af1f20788f07918a30cd694
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-01-20 10:33:21 -0700

    test changes to frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,15 @@
-import { Route, Link, Switch } from 'react-router-dom';
-import './App.css';
-import Comments from './components/Comments.jsx';
-import Home from './components/Home.jsx';
-import NewBlog from './components/New-blog.jsx';
-import Show from './components/Show.jsx';
-require('dotenv').config();
+import { Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+import Comments from "./components/Comments.jsx";
+import Home from "./components/Home.jsx";
+import NewBlog from "./components/New-blog.jsx";
+import Show from "./components/Show.jsx";
+require("dotenv").config();
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey There</h1>
+      <h1>Let's make a change.</h1>
       <Comments />
       <Home />
       <NewBlog />
